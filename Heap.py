4babdf3eabd462942202b9359899dcd09515c1d5
parsabee <35123665+parsabee@users.noreply.github.com>
2019-03-03 22:07:12 -0800

    bounded priority queue container

---
+++ Heap.py
@@ -0,0 +1,132 @@
+__author__ = 'Parsa Bagheri'
+'''
+MIT License
+
+Copyright (c) 2019, Parsa Bagheri
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+'''
+
+# A Generic container that uses the heap datastructure
+# It will be used to create priority queues
+
+class Heap(object):
+    def __init__(self, size, cmpFxn):
+        self.__maxSize = size
+        self.__length = 0
+        self.__heap = [None] * (size + 1)
+        self.__cmp = cmpFxn
+
+    def __str__ (self):
+        s = ""
+        for i in range (1, self.__length):
+            s += str(self.__heap[i])
+            s += ", "
+        s += str(self.__heap[self.__length])
+        return s
+
+    def getHeap(self):
+        return self.__heap
+
+    def getMaxSize(self):
+        return self.__maxSize
+    
+    def setMaxSize(self, ms):
+        if ms <= self.__maxSize:
+            return 0
+        new_heap = [None] * (ms - self.__maxSize)
+        self.__heap += new_heap
+        self.__maxSize = ms
+        return 1
+
+    def getLength(self):
+        return self.__length
+
+    def __getParent(self, i):
+        assert i >= 1
+        if i == 1:
+            return 1
+        return i//2
+
+    def __getLeft(self, i):
+        return i * 2
+
+    def __getRight(self, i):
+        return (i * 2) + 1
+
+    def __swap(self, i, j):
+        tmp = self.__heap[i]
+        self.__heap[i] = self.__heap[j]
+        self.__heap[j] = tmp
+
+    def insert(self, data):
+        try:
+            self.__length += 1
+            if self.__length > self.__maxSize:
+                self.__length -= 1
+                return 0
+            self.__heap [self.__length] = data
+            i = self.__length
+            ip = self.__getParent(i)
+            while self.__cmp(self.__heap[i], self.__heap[ip]):
+                self.__swap(i, ip)
+                i = ip
+                ip = self.__getParent(ip)
+            return 1
+        except:
+            print('there is no way to comare the inserted objects\n' \
+                'suggestion: you may need to define the \'__eq__\' and \'__lt__\' operators for the class of the object you are inserting')
+            return 0
+
+    def top(self):
+        if self.__length < 1:
+            return None
+        return self.__heap[1]
+
+    def extract(self):
+        if self.__length < 1:
+            return None
+        r = self.__heap[1]
+        self.__swap(1, self.__length)
+        self.__heap[self.__length] = None
+        self.__length -= 1
+        if self.__length > 1:
+            self.__heapify()
+        return r
+
+    def __heapify(self):
+        n = 1
+        while n < self.__length:
+            l = self.__getLeft(n)
+            r = self.__getRight(n)
+            try:
+                rp = l
+                if self.__heap[r] != None:
+                    if self.__cmp(self.__heap[r], self.__heap[l]):
+                        rp = r
+                if rp > self.__length:
+                    break
+            except IndexError:
+                break
+                    
+            if self.__cmp(self.__heap[rp], self.__heap[n]):
+                self.__swap(rp, n)
+                n = rp
+            else:
+                break
---
+++ pQueue.py
@@ -0,0 +1,105 @@
+__author__ = 'Parsa Bagheri'
+'''
+MIT License
+
+Copyright (c) 2019, Parsa Bagheri
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+'''
+from Heap import *
+
+# A generic bounded priority queue container
+
+class pQueue(object):
+    '''
+    bounded priority queue
+    uses a heap data structure
+    '''
+    def __init__(self, size = 10, cmpFxn = lambda x,y: x > y) :
+        '''
+        two arguments:
+        (1) size of the priority queue
+            default is 10
+
+        (2) compare function:
+            compare function is used to determine the priority of inserted elements
+            example of a compare function:
+            def cmpFxn (x, y):
+                return x > y
+
+            function above is the defualt compare function, which produces a max heap
+
+        NB if you are making a priority queue to contain user defined classes,
+            you need to make sure that your compare function is capable of comapring the objects of that class
+        '''
+        self.__myHeap = Heap(size, cmpFxn)
+
+    def setSize(self, size) :
+        '''
+        update the size of the queue,
+        new size has to be greater than the old size
+        if successful returns 1
+        else (new size < old size) returns 0
+        '''
+        return self.__myHeap.setMaxSize(size)
+        
+    def insert(self, data):
+        '''
+        inserts an element in the heap if there is room for insertion
+        if successful returns 1
+        else (queue is full) returns 0
+        '''
+        return self.__myHeap.insert(data)
+        
+    def top(self):
+        '''
+        returns the first element of the queue,
+        None if the queue is empty
+        '''
+        return self.__myHeap.top()
+    
+    def pop(self):
+        '''
+        removes and returns the first element of the queue
+        None if the queue is empty
+        '''
+        return self.__myHeap.extract()
+
+    def isEmpty(self):
+        '''
+        returns True if the queue is empty, False otherwise
+        '''
+        return self.__myHeap.getLength() <= 1
+    
+    def __str__(self):
+        '''
+        prints the element of the queue
+
+        NB To use this function, you need to make sure that the class of the object that is being inserted to the queue,
+           has the __str__ method defined.
+        '''
+        try:
+            s = str(self.__myHeap)
+            return ("Current Queue: " + s)
+        except:
+            print('the \'__str__\' method is not defined for the objects in the queue')
+            return ''
+
+
+    
